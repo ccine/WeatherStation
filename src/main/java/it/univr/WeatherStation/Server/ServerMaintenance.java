9affe27b1fb99c641db4bd8b63908a629fb28ced
ccine <56122695+ccine@users.noreply.github.com>
2022-01-17 18:00:16 +0100

    Modifiche a server e rispettivi test

--- src/main/java/it/univr/WeatherStation/Server/ServerMaintenance.java
+++ src/main/java/it/univr/WeatherStation/Server/ServerMaintenance.java
@@ -2,18 +2,44 @@ package it.univr.WeatherStation.Server;
 
 import org.json.JSONException;
 
+import java.sql.Timestamp;
+
 public class ServerMaintenance extends Server {
 
     public StationState getLastState(){
         try {
-            return new StationState(
-                    (lastValue.get("timestamp")).toString(),
-                    Integer.toString((Integer) lastValue.get("station")),
-                    (String) lastValue.get("batteryLevel"),
-                    Boolean.toString((Boolean) lastValue.get("isCharging")),
-                    Boolean.toString((Boolean) lastValue.get("energySaving"))
-            );
-        } catch (JSONException | NullPointerException e) {
+            String timestamp;
+            String station;
+            String batteryLevel;
+            String isCharging;
+            String energySaving;
+            try {
+                timestamp = ((Timestamp) lastValue.get("timestamp")).toString();
+            } catch (JSONException jex) {
+                timestamp = "";
+            }
+            try {
+                station = Integer.toString((Integer) lastValue.get("station"));
+            } catch (JSONException jex) {
+                station = "";
+            }
+            try {
+                batteryLevel = (String) lastValue.get("batteryLevel");
+            } catch (JSONException jex) {
+                batteryLevel = "";
+            }
+            try {
+                isCharging = Boolean.toString((Boolean) lastValue.get("isCharging"));
+            } catch (JSONException jex) {
+                isCharging = "";
+            }
+            try {
+                energySaving = Boolean.toString((Boolean) lastValue.get("energySaving"));
+            } catch (JSONException jex) {
+                energySaving = "";
+            }
+            return new StationState(timestamp, station, batteryLevel, isCharging, energySaving);
+        } catch (NullPointerException e) {
             return new StationState();
         }
     }
--- src/test/java/it/univr/WeatherStation/Server/ServerMaintenanceTest.java
+++ src/test/java/it/univr/WeatherStation/Server/ServerMaintenanceTest.java
@@ -12,16 +12,32 @@ public class ServerMaintenanceTest {
 
     @Test
     public void getLastState() throws JSONException {
+        ServerMaintenance sm = new ServerMaintenance();
+        StationState ss = sm.getLastState();
+        assertEquals("", ss.getTimestamp());
+        assertEquals("", ss.getStation());
+        assertEquals("", ss.getBatteryLevel());
+        assertEquals("", ss.getIsCharging());
+        assertEquals("", ss.getEnergySaving());
+
         JSONObject json = new JSONObject();
+
+        sm.sendData(json);
+        ss = sm.getLastState();
+        assertEquals("", ss.getTimestamp());
+        assertEquals("", ss.getStation());
+        assertEquals("", ss.getBatteryLevel());
+        assertEquals("", ss.getIsCharging());
+        assertEquals("", ss.getEnergySaving());
+
         Timestamp ts = new Timestamp(System.currentTimeMillis());
         json.put("timestamp", ts);
         json.put("station", 1234);
         json.put("batteryLevel", "10");
         json.put("isCharging", true);
         json.put("energySaving", false);
-        ServerMaintenance sm = new ServerMaintenance();
         sm.sendData(json);
-        StationState ss = sm.getLastState();
+        ss = sm.getLastState();
         assertEquals(ts.toString(), ss.getTimestamp());
         assertEquals("1234", ss.getStation());
         assertEquals("10", ss.getBatteryLevel());
